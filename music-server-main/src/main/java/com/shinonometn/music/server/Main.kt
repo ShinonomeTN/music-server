4751bd9db2d78575fe8e1b2072077ab7f5def7d3
CattenLinger <cattenlinger@outlook.com>
2022-07-12 00:37:08 +0800

    Upgrade koemans version to 1.2-SNAPSHOT Use github package source Upgrade kotlin to 1.6.0 Upgrade ktor to 1.6.8

--- music-server-main/src/main/java/com/shinonometn/music/server/Main.kt
+++ music-server-main/src/main/java/com/shinonometn/music/server/Main.kt
@@ -2,9 +2,9 @@ package com.shinonometn.music.server
 
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
-import com.shinonometn.koemans.getEnvironmentHoconConfig
 import com.shinonometn.koemans.spring.propertySourcePlaceholderSupport
 import com.shinonometn.koemans.spring.useHoconPropertySource
+import com.shinonometn.koemans.web.getEnvironmentHoconConfig
 import com.shinonometn.koemans.web.spring.configuration.configureBySpring
 import com.shinonometn.music.server.platform.configuration.PlatformAutoConfiguration
 import io.ktor.application.*
